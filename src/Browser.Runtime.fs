62a9018fba4d4f6f8c1c57e7fbce74bf058e8648
Lucien "Mingxi" Du <david.xelnaga@gmail.com>
2022-08-02 15:06:07 -0700

    laid out message protacal

---
+++ src/Browser.Runtime.fs
@@ -0,0 +1,26 @@
+﻿module Browser
+open Fable.Core
+module Runtime =
+    module Types =
+        
+        type MessageSender = {
+            tab: obj option
+            frameId: int option
+
+        }
+        type FuncSendRespond<'Msg> = 'Msg -> unit
+        type MessageListener<'Msg, 'Res> = 'Msg * MessageSender * FuncSendRespond<'Res> -> bool
+        type AsyncMessageListener<'Msg, 'Res> = 'Msg * MessageSender -> JS.Promise<'Res>
+        type MessageTarget =
+            abstract addListener: MessageListener<'Msg, 'Res> -> unit
+
+        type IMessagePort =
+            abstract sendMessage: message:'Msg * ?options: 'Opt  -> JS.Promise<'Res>
+            abstract sendMessage: id:int * message:'Msg * options: 'Opt  -> JS.Promise<'Res>
+
+        type IRuntime =
+            inherit IMessagePort
+
+open Runtime.Types
+
+let [<Global("brower.runtime")>] Runtime: IRuntime = jsNative
--- src/Content.fs
+++ src/Content.fs
@@ -1,1 +1,12 @@
 module FableFirefoxExtensionSample.Content
+
+open Browser.Runtime
+open Fable.Core
+
+do
+    promise{
+        let! result = Browser.Runtime.sendMessage(123)
+        match result with
+        |"test" -> ()
+        |_ -> invalidOp "Unexpected message"
+    }|>Promise.start
